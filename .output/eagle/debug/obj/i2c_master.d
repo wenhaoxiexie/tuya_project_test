935559a2fd9210e3e6b226e6629ebd009a31f059
xiewenhao <wenhao_xie@126.com>
2020-06-05 16:46:52 +0800

    first commit

---
+++ .output/eagle/debug/obj/i2c_master.d
@@ -0,0 +1,10 @@
+../../../../.output/eagle/debug/obj/i2c_master.o ../../../../.output/eagle/debug/obj/i2c_master.d : i2c_master.c ../../../../include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../../../../include/espressif/esp8266/ets_sys.h \
+ ../../../../include/espressif/esp8266/gpio_register.h \
+ ../../../../app/tuya_common/include/gpio.h ../include/i2c_master.h \
+ ../../../../include/espressif/esp8266/pin_mux_register.h \
+ ../../../../include/espressif/esp8266/eagle_soc.h \
+ ../../../../include/espressif/esp8266/eagle_soc.h
---
+++ .output/eagle/debug/obj/mir3da.d
@@ -0,0 +1,9 @@
+../../../../.output/eagle/debug/obj/mir3da.o ../../../../.output/eagle/debug/obj/mir3da.d : mir3da.c ../../../../include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../../../../include/espressif/esp_libc.h \
+ ../../../../app/tuya_common/include/system/uni_log.h \
+ ../../../../app/tuya_common/include/com_def.h \
+ ../../../../app/tuya_common/include/error_code.h \
+ ../../../../app/tuya_common/include/com_def.h ../include/mir3da.h
